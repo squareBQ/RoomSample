1935a5f8e066497f680e6dc034b3697db902cfad
zashine <zahileoo@gmail.com>
2025-08-19 10:37:44 +0800

    add room model and dao interface

---
+++ app/src/main/java/com/mt/roomsample/dao/WordDao.kt
@@ -0,0 +1,33 @@
+package com.mt.roomsample.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mt.roomsample.model.Word
+
+/**
+ * time : 8/19/25
+ *
+ * - WordDao 是一个接口，DAO必须是接口或抽象类
+ * - @Dao 注解将其标识为 Room 的 DAO 类
+ * - suspend fun insert(word: Word) 声明挂起函数以插入一个字词
+ * - @Insert 注解是一种特殊的 DAO 方法注解，使用 DAO 方法注解时，您无需提供任何 SQL 代码。
+ * - onConflict = OnConflictStrategy.IGNORE 所选 onConflict 策略将忽略与列表中的现有字词完全相同的新字词
+ * - suspend fun deleteAll()：声明挂起函数以删除所有字词
+ * - @Query("DELETE FROM word_table")：@Query 要求您将 SQL 查询作为字符串参数提供给注解，以执行复杂的读取查询及其他操作。
+ * - @Query 注解声明一个返回字词列表的查询方法
+ * - @Query("SELECT * FROM word_table ORDER BY word ASC")：可返回按升序排序的字词列表的查询。
+ */
+@Dao
+interface WordDao {
+
+	@Query("SELECT * from word_table ORDER BY word ASC")
+	fun getAlphabetizedWords(): List<Word>
+
+	@Insert(onConflict = OnConflictStrategy.IGNORE)
+	suspend fun insert(word: Word)
+
+	@Query("DELETE FROM word_table")
+	suspend fun deleteAll()
+}
---
+++ app/src/main/java/com/mt/roomsample/model/Word.kt
@@ -0,0 +1,21 @@
+package com.mt.roomsample.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * time : 8/19/25
+ * desc : Word实体
+ *
+ * @Entity(tableName = "word_table")  每个 @Entity 类代表一个表。为你的类定义的类名就是表名。如果不显示指定表名，则默认表名就是类名。
+ * @PrimaryKey 每个实体都需要主键。通常主键是自增长或非空字段。为简便起见，每个字词都可充当自己的主键。
+ * @ColumnInfo(name = "word") 如果您希望该表中列的名称与成员变量的名称不同，可以指定表中列的名称，此处的列名为“word”。
+ *
+ * @PrimaryKey(autoGenerate = true) 可自动生成唯一的键, 自增长
+ */
+@Entity(tableName = "word_table")
+class Word(
+	@PrimaryKey(autoGenerate = true) val id: Int,
+	@ColumnInfo(name = "word") val word: String,
+)
